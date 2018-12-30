78ffddd96584f703bb736afc0c899c8a2ecbd93e
Pranav J.I <pranav@localhost.localdomain>
2018-12-31 00:27:07 +0530

    First

---
+++ src/main/java/FileGetter.java
@@ -0,0 +1,67 @@
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pranav
+ */
+public class FileGetter {
+   
+     static void RecursivePrint(String path,PrintWriter out)  
+     { 
+         String host="localhost";
+	    String user="pranav";
+	    String password="Login!@34";
+            out.println(path);
+	    String command1="tree -dH baseHREF -L 1 "+path;
+	    try{
+	    	
+	    	java.util.Properties config = new java.util.Properties(); 
+	    	config.put("StrictHostKeyChecking", "no");
+	    	JSch jsch = new JSch();
+	    	Session session=jsch.getSession(user, host, 22);
+	    	session.setPassword(password);
+	    	session.setConfig(config);
+	    	session.connect();
+	    
+	    	
+	    	Channel channel=session.openChannel("exec");
+	        ((ChannelExec)channel).setCommand(command1);
+	        channel.setInputStream(null);
+	        ((ChannelExec)channel).setErrStream(System.err);
+	        
+	        InputStream in=channel.getInputStream();
+	        channel.connect();
+	        byte[] tmp=new byte[1024];
+	        while(true){
+	          while(in.available()>0){
+	            int i=in.read(tmp, 0, 1024);
+	            if(i<0)break;
+	            out.println(new String(tmp, 0, i).replaceAll("\n","</br>")+"<br>");
+	          }
+	          if(channel.isClosed()){
+	            //out.println("exit-status: "+channel.getExitStatus());
+	            break;
+	          }
+	          try{Thread.sleep(1000);}catch(Exception ee){}
+	        }
+	        channel.disconnect();
+	        session.disconnect();
+	        out.println("DONE");
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }
+        
+     }
+}
